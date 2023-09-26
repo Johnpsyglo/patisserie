21078d1172d9630ec6f16e194fb260f1e353591d
ceci est un test <johngloire@gmail.com>
2023-09-26 15:01:59 +0400

    tout est ok

---
+++ php/pattiserie.php
@@ -0,0 +1,49 @@
+<?php
+// include('');
+
+// session_start([
+//     'cookie_lifetime' => 5 // Durée de vie du cookie de session (en secondes)
+// ]);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog Plateforme Pâtisserie</title><link rel="stylesheet" href="demoteststyle.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+
+</head>
+<body>
+<header>
+        <h1>Blog Pâtisserie</h1>
+        <nav>
+            <ul>
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">Articles</a></li>
+                <li><a href="#">A propos</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <article>
+            <h2>Titre de l'article</h2>
+            <p>Date de publication</p>
+            <p>Contenu de l'article...</p>
+        </article>
+        <!-- Répéter la structure pour d'autres articles -->
+    </main>
+    
+    <footer>
+        <p>&copy; 2023 Blog Call of Duty. Tous droits réservés.</p>
+    </footer>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
